2aa47c4065f9566d9bb6e59b3cde1eb3cdc6fbda
csbhr <baihaoran@njust.edu.cn>
2020-03-23 23:47:11 +0800

    clear

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,88 +1,9 @@
-import random
 import torch
 import torch.nn.functional as F
 import numpy as np
 import math
 from torch.autograd import Variable
 import cv2
-
-
-def get_patch(*args, patch_size=17, scale=1):
-    """
-    Get patch from an image
-    """
-    ih, iw, _ = args[0].shape
-
-    ip = patch_size
-    tp = scale * ip
-
-    ix = random.randrange(0, iw - ip + 1)
-    iy = random.randrange(0, ih - ip + 1)
-    tx, ty = scale * ix, scale * iy
-
-    ret = [
-        args[0][iy:iy + ip, ix:ix + ip, :],
-        *[a[ty:ty + tp, tx:tx + tp, :] for a in args[1:]]
-    ]
-
-    return ret
-
-
-def np2Tensor(*args, rgb_range=255):
-    def _np2Tensor(img):
-        img = img.astype('float64')
-        np_transpose = np.ascontiguousarray(img.transpose((2, 0, 1)))  # HWC -> CHW
-        tensor = torch.from_numpy(np_transpose).float()  # numpy -> tensor
-        tensor.mul_(rgb_range / 255)  # (0,255) -> (0,1)
-
-        return tensor
-
-    return [_np2Tensor(a) for a in args]
-
-
-def data_augment(*args, hflip=True, rot=True):
-    hflip = hflip and random.random() < 0.5
-    vflip = rot and random.random() < 0.5
-    rot90 = rot and random.random() < 0.5
-
-    def _augment(img):
-        if hflip:
-            img = img[:, ::-1, :]
-        if vflip:
-            img = img[::-1, :, :]
-        if rot90:
-            img = np.rot90(img)
-
-        return img
-
-    return [_augment(a) for a in args]
-
-
-def postprocess(*images, rgb_range):
-    def _postprocess(img, rgb_coefficient):
-        img = img.mul(rgb_coefficient).clamp(0, 255).round()
-
-        img = img[0].data.cpu().numpy()  # BCHW -> CHW
-        img = np.transpose(img, (1, 2, 0)).astype(np.uint8)  # CHW -> HWC
-
-        return img
-
-    rgb_coefficient = 255 / rgb_range
-    return [_postprocess(img, rgb_coefficient) for img in images]
-
-
-def calc_psnr(img1, img2, rgb_range=1., shave=4):
-    if isinstance(img1, torch.Tensor):
-        img1 = img1[:, :, shave:-shave, shave:-shave]
-        img1 = img1.to('cpu').numpy()
-    if isinstance(img2, torch.Tensor):
-        img2 = img2[:, :, shave:-shave, shave:-shave]
-        img2 = img2.to('cpu').numpy()
-    mse = np.mean((img1 / rgb_range - img2 / rgb_range) ** 2)
-    if mse == 0:
-        return 100
-    PIXEL_MAX = 1
-    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 
 def calc_grad_sobel(img, device='cuda'):
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,6 +1,4 @@
 import os
-import glob
-import cv2
 
 
 def handle_dir(dir):
@@ -32,42 +30,3 @@ def zip_frames_to_videos(ori_root, save_root, fname_template='%4d.png', video_ex
         command = 'ffmpeg -r 24000/1001 -i {} -vcodec libx265 -pix_fmt yuv422p -crf 10 {}'.format(imgs_path, video_path)
         os.system(command)
         print("zip frames to {}".format(video_path))
-
-
-def resize_videos(ori_root, save_root, scale):
-    handle_dir(save_root)
-    videos_path = sorted(glob.glob(os.path.join(ori_root, "*")))
-    for vp in videos_path:
-        video_name = os.path.basename(vp)
-        imgs_path = sorted(glob.glob(os.path.join(vp, "*")))
-        for ip in imgs_path:
-            img_name = os.path.basename(ip)
-            img = cv2.imread(ip, cv2.IMREAD_COLOR)
-            h, w, c = img.shape
-            bicubic_img = cv2.resize(img, (int(w * scale), int(h * scale)), cv2.INTER_CUBIC)
-
-            savepath = os.path.join(save_root, video_name)
-            handle_dir(savepath)
-            cv2.imwrite(os.path.join(savepath, img_name), bicubic_img)
-            print('resize img:', ip)
-
-
-# ori_root = '/media/csbhr/1_Disk2T/BHR/Dataset/SR_DataSet/AI+4K_HDR/video/SDR_540p'
-# save_root = '/media/csbhr/2_Disk2T/BHR/Dataset/SR_DataSet/AI+4K_HDR/png/LR'
-# ext_frames_from_videos(ori_root, save_root, start_idx=300, end_idx=700)
-#
-# ori_root = '/media/csbhr/1_Disk2T/BHR/Dataset/SR_DataSet/AI+4K_HDR/video/SDR_4K'
-# save_root = '/media/csbhr/2_Disk2T/BHR/Dataset/SR_DataSet/AI+4K_HDR/png/GT'
-# ext_frames_from_videos(ori_root, save_root, start_idx=300, end_idx=700)
-
-# ori_root = '/media/csbhr/1_Disk2T/BHR/Dataset/SR_DataSet/AI+4K_HDR/初赛/测试集/SDR_540p'
-# save_root = '/media/csbhr/2_Disk2T/BHR/Dataset/SR_DataSet/AI+4K_HDR/test_first/LR'
-# ext_frames_from_videos(ori_root, save_root)
-
-ori_root = '/media/csbhr/2_Disk2T/BHR/Dataset/SR_DataSet/REDS/train_240_269/HR'
-save_root = '/media/csbhr/2_Disk2T/BHR/Dataset/SR_DataSet/REDS/train_240_269/LR'
-resize_videos(ori_root, save_root, scale=0.25)
-
-# ori_root = '/media/csbhr/1_Disk2T/temp/test_first/results/sharp_bicubic/png3'
-# save_root = '/media/csbhr/2_Disk2T/temp/results/sharp_bicubic/video2'
-# zip_frames_to_videos(ori_root, save_root)
