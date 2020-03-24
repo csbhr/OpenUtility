a64c57b52104a56305b6aa1a639087a542b133ef
csbhr <baihaoran@njust.edu.cn>
2020-03-24 17:20:43 +0800

    update readme

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -14,8 +14,10 @@ def batch_resize_images(ori_root, dest_root, scale=1.0, postfix=True):
         postfix: if True: add scale as postfix for filename after resizing
     '''
     handle_dir(dest_root)
+    scale = float(scale)
     if postfix:
-        dfile_template = "{}_"+str(scale)+".png"
+        pf = "{}d{}".format(str(scale).split(".")[0], str(scale).split(".")[1])
+        dfile_template = "{}_"+pf+".png"
     else:
         dfile_template = "{}.png"
     images_fname = sorted(os.listdir(ori_root))
