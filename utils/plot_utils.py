b755d9e5ca43d2fb34c9cc77ddd0e409c1597049
csbhr <baihaoran@njust.edu.cn>
2020-10-13 20:58:38 +0800

    update utils/plot_utils.py

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -88,3 +88,43 @@ def plot_multi_curve(array_list, label_list, title='compare', xlabel='Epochs', y
         plt.close(fig)
     else:
         plt.show()
+
+
+def plot_multi_curve_given_axis(array_list, label_list, axis, title='compare', xlabel='Epochs', ylabel=None, save=None):
+    '''
+    function:
+        plot multi curve in one figure
+    required params:
+        array_list: the data of curves, a list
+            each item should be numpy.array, ndim=1
+        label_list: list, labels for each curve
+        axis: list, x-axis flags
+    optional params:
+        title: the title of figure
+        xlabel: the flag of x-axis
+        ylabel: the flag of y-axis, if None: ylabel=title
+        save:
+            if None: just show figure
+            else: the path to save figure, the figure will be saved
+    '''
+    assert len(array_list) == len(label_list), "length not equal"
+
+    fig = plt.figure()
+    plt.title(title)
+
+    # plot curves
+    for i in range(len(array_list)):
+        plt.plot(axis, array_list[i], label=label_list[i])
+
+    plt.legend()
+    plt.xlabel(xlabel)
+    if not ylabel:
+        ylabel = title
+    plt.ylabel(ylabel)
+    plt.grid(True)
+
+    if save:
+        plt.savefig(save)
+        plt.close(fig)
+    else:
+        plt.show()
